81022d8118c52a0cbcfdbcabd20987cd361d72a2
Sarabjeet Singh <sarabsingh212@gmail.com>
2019-09-12 15:29:41 +0530

    typo fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcmoe to React Chandigarh!
+          Welcome to React Chandigarh!
         </p>
         <a
           className="App-link"
