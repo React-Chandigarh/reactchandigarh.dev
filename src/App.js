573871018972fcc81c9c6b96bc6beefd0c62b672
Sarabjeet Singh <sarabsingh212@gmail.com>
2019-11-19 22:55:41 +0530

    Homepage with past events

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Navigation from './Navigation';
 import AboutUs from './components/About';
+import Home from './components/Home/';
 
 function App() {
   return (
@@ -17,22 +17,7 @@ function App() {
         <Route path="/team" component={() => <p>Team</p>} />
         <Route path="/news" component={() => <p>News</p>} />
         <Route path="/blogs" component={() => <p>Blogs</p>} />
-        <Route path="/" component={() => (
-          <>
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Welcome to React Chandigarh!
-            </p>
-            <a
-              className="App-link"
-              href="https://www.meetup.com/Chandigarh-React-Developers/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Visit our meetup group.
-            </a>
-          </>
-        )}
+        <Route path="/" component={Home}
         />
       </Switch>
     </div>
