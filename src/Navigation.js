48b3402b5fcee2314586bce6e2ef35d2fa3b29db
Sarabjeet Singh <sarabsingh212@gmail.com>
2019-11-20 21:19:22 +0530

    Removed unused links from navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -6,10 +6,7 @@ const Navigation = () => (
     <ul>
         <li><Link to="/home">Home</Link></li>
         <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/news">News</Link></li>
-        <li><Link to="/team">The Team</Link></li>
         <li><Link to="/events">Events</Link></li>
-        <li><Link to="/contact">Contact Us</Link></li>
     </ul>
 );
 
