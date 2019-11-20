1cf7d1204c8984544f9f3ae1cb4aa44d6eaf62c6
Sarabjeet Singh <sarabsingh212@gmail.com>
2019-11-20 22:43:35 +0530

    feedback form link

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,6 +16,17 @@ const Home = () => (
         >
             Visit our meetup group.
         </a>
+        <br />
+        <br />
+        <a
+            className="App-link App-link-button"
+            href="https://forms.gle/sjaaCwG65zVtKsMF6"
+            target="_blank"
+            rel="noopener noreferrer"
+        >Please let us know if you have any Feedback for us.</a>
+        <br />
+        <br />
+        <p className="App-link">Email us at reactdevchd@gmail.com</p>
     </>
 );
 
